c811f70e75ba86704a7b12b904c7c4957b8c0115
wienni112 <55889937+wienni112@users.noreply.github.com>
2025-05-27 21:35:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/ash
-cd /home/container
 
-MODIFIED_STARTUP=$(eval echo "$STARTUP")
-echo ":/home/container$ $MODIFIED_STARTUP"
+# Set defaults if not defined
+SERVER_IP="${SERVER_IP:-1.1.1.1}"
+RCON_PORT="${RCON_PORT:-123456}"
+RCON_PASSWORD="${RCON_PASSWORD:-abcdef}"
+MIDCAP_LIMIT="${MIDCAP_LIMIT:-50}"
+LASTCAP_LIMIT="${LASTCAP_LIMIT:-70}"
 
-exec $MODIFIED_STARTUP
+echo "🔧 Konfiguration wird angepasst..."
+sed -i "s/1.1.1.1/${SERVER_IP}/g" /opt/app/seeding.midcap.yml
+sed -i "s/123456/${RCON_PORT}/g" /opt/app/seeding.midcap.yml
+sed -i "s/abcdef/${RCON_PASSWORD}/g" /opt/app/seeding.midcap.yml
+sed -i "s/50/${MIDCAP_LIMIT}/g" /opt/app/seeding.midcap.yml
+
+sed -i "s/1.1.1.1/${SERVER_IP}/g" /opt/app/seeding.lastcap.yml
+sed -i "s/123456/${RCON_PORT}/g" /opt/app/seeding.lastcap.yml
+sed -i "s/abcdef/${RCON_PASSWORD}/g" /opt/app/seeding.lastcap.yml
+sed -i "s/70/${LASTCAP_LIMIT}/g" /opt/app/seeding.lastcap.yml
+
+echo "🚀 Starte hll-geofences..."
+exec /opt/app/hll-geofences
