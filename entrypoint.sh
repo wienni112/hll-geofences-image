9ccac1f104b1b619d64bd624836beecb5c3b007f
wienni112 <55889937+wienni112@users.noreply.github.com>
2025-05-27 21:45:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/ash
+cd /home/container
 
-# Set defaults if not defined
-SERVER_IP="${SERVER_IP:-1.1.1.1}"
-RCON_PORT="${RCON_PORT:-123456}"
-RCON_PASSWORD="${RCON_PASSWORD:-abcdef}"
-MIDCAP_LIMIT="${MIDCAP_LIMIT:-50}"
-LASTCAP_LIMIT="${LASTCAP_LIMIT:-70}"
+# Konfiguration anpassen, aber nur wenn beschreibbar
+if [ -w /home/container ]; then
+    echo "🔧 Konfiguration wird angepasst..."
+    sed -i "s/1.1.1.1/${HLL_SERVER_IP}/g" seeding.midcap.yml
+    sed -i "s/123456/${RCON_PORT}/g" seeding.midcap.yml
+    sed -i "s/abcdef/${RCON_PASSWORD}/g" seeding.midcap.yml
+    sed -i "s/50/${MIDCAP_LIMIT}/g" seeding.midcap.yml
 
-echo "🔧 Konfiguration wird angepasst..."
-sed -i "s/1.1.1.1/${SERVER_IP}/g" /opt/app/seeding.midcap.yml
-sed -i "s/123456/${RCON_PORT}/g" /opt/app/seeding.midcap.yml
-sed -i "s/abcdef/${RCON_PASSWORD}/g" /opt/app/seeding.midcap.yml
-sed -i "s/50/${MIDCAP_LIMIT}/g" /opt/app/seeding.midcap.yml
-
-sed -i "s/1.1.1.1/${SERVER_IP}/g" /opt/app/seeding.lastcap.yml
-sed -i "s/123456/${RCON_PORT}/g" /opt/app/seeding.lastcap.yml
-sed -i "s/abcdef/${RCON_PASSWORD}/g" /opt/app/seeding.lastcap.yml
-sed -i "s/70/${LASTCAP_LIMIT}/g" /opt/app/seeding.lastcap.yml
+    sed -i "s/1.1.1.1/${HLL_SERVER_IP}/g" seeding.lastcap.yml
+    sed -i "s/123456/${RCON_PORT}/g" seeding.lastcap.yml
+    sed -i "s/abcdef/${RCON_PASSWORD}/g" seeding.lastcap.yml
+    sed -i "s/70/${LASTCAP_LIMIT}/g" seeding.lastcap.yml
+fi
 
 echo "🚀 Starte hll-geofences..."
-exec /opt/app/hll-geofences
+exec ./hll-geofences
